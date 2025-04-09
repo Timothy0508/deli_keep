42ab0769e6df7334ab3873537734fe218447b5db
Timothy0508 <99249271+Timothy0508@users.noreply.github.com>
2025-04-09 13:59:28 +0800

    feat: add color to title and icon

--- lib/widgets/type_card.dart
+++ lib/widgets/type_card.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
 
 enum PredefineTypes {
-  stared(icon: Icon(Icons.star_border_outlined), title: 'Starred'),
-  today(icon: Icon(Icons.today_outlined), title: 'Today'),
-  planned(icon: Icon(Icons.calendar_month_outlined), title: 'Planned'),
-  tasks(icon: Icon(Icons.home_outlined), title: 'Tasks');
+  stared(
+    icon: Icon(Icons.star_border_outlined, color: Colors.deepOrange),
+    title: 'Starred',
+    color: Colors.deepOrange,
+  ),
+  today(
+    icon: Icon(Icons.today_outlined, color: Colors.blue),
+    title: 'Today',
+    color: Colors.blue,
+  ),
+  planned(
+    icon: Icon(Icons.calendar_month_outlined, color: Colors.green),
+    title: 'Planned',
+    color: Colors.green,
+  ),
+  tasks(
+    icon: Icon(Icons.home_outlined, color: Colors.purple),
+    title: 'Tasks',
+    color: Colors.purple,
+  );
 
   final Icon icon;
 
   final String title;
 
-  const PredefineTypes({required this.icon, required this.title});
+  final dynamic color;
+
+  const PredefineTypes({
+    required this.icon,
+    required this.title,
+    required this.color,
+  });
 }
 
 class TypeCard extends StatefulWidget {
@@ -26,9 +48,19 @@ class _TypeCardState extends State<TypeCard> {
   @override
   Widget build(BuildContext context) {
     Icon icon = widget.type.icon;
-    Text title = Text(widget.type.title);
+    Text title = Text(
+      widget.type.title,
+      style: TextStyle(color: widget.type.color),
+    );
 
-    var body = Card(child: Container(child: Row(children: [icon, title])));
+    var body = Card(
+      child: Container(
+        child: Row(
+          children: [icon, title],
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        ),
+      ),
+    );
 
     return body;
   }
