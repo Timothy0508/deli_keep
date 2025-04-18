a021df226cddfa215d90d0171d0ac7a50d962e6d
Timothy0508 <99249271+Timothy0508@users.noreply.github.com>
2025-04-18 12:57:22 +0800

    feat: automatically generate items.
    **I hope it'll run properly...**

--- lib/routes/note_page.dart
+++ lib/routes/note_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../models/note.dart';
 import '../widgets/add_note_dialog.dart';
+import '../widgets/note_container.dart';
 
 ///The page for each categories
 ///
@@ -20,12 +22,15 @@ class NotePage extends StatefulWidget {
 
   final Icon icon;
 
+  final List<Note> notes;
+
   const NotePage({
     super.key,
     required this.title,
     required this.color,
     required this.backgroundColor,
     required this.icon,
+    required this.notes,
   });
 
   @override
@@ -36,37 +41,42 @@ class _NotePageState extends State<NotePage> {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-
-    var body = CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          title: Text(widget.title),
-          backgroundColor:
-              Theme.of(context)
-                  .copyWith(
-                    colorScheme: ColorScheme.fromSeed(seedColor: widget.color),
-                  )
-                  .colorScheme
-                  .inversePrimary,
-          expandedHeight: 200,
-          flexibleSpace: Container(
-            alignment: Alignment.bottomLeft,
-            margin: EdgeInsets.all(20),
-            height: size.height,
-            child: Row(
-              spacing: 20,
-              children: [
-                Icon(widget.icon.icon, size: 40, color: widget.color),
-                Text(
-                  widget.title,
-                  style: TextStyle(color: widget.color, fontSize: 40),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
+    List<Widget> notes = List.generate(
+      widget.notes.length,
+      (index) =>
+          NoteContainer(note: widget.notes[index], themeColor: widget.color),
     );
+
+    var appBar = SliverAppBar(
+      title: Text(widget.title),
+      backgroundColor:
+          Theme.of(context)
+              .copyWith(
+                colorScheme: ColorScheme.fromSeed(seedColor: widget.color),
+              )
+              .colorScheme
+              .inversePrimary,
+      expandedHeight: 200,
+      flexibleSpace: Container(
+        alignment: Alignment.bottomLeft,
+        margin: EdgeInsets.all(20),
+        height: size.height,
+        child: Row(
+          spacing: 20,
+          children: [
+            Icon(widget.icon.icon, size: 40, color: widget.color),
+            Text(
+              widget.title,
+              style: TextStyle(color: widget.color, fontSize: 40),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    var notesShowcase = SliverList(delegate: SliverChildListDelegate(notes));
+
+    var body = CustomScrollView(slivers: [appBar, notesShowcase]);
 
     return Theme(
       data: ThemeData(
