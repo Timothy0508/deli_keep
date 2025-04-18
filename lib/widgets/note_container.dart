9a6fd61e32154edc1f26a44c0f05516ae06e110b
Timothy0508 <99249271+Timothy0508@users.noreply.github.com>
2025-04-18 12:45:50 +0800

    feat: create widgets to show notes

---
+++ lib/widgets/note_container.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../models/note.dart';
+
+class NoteContainer extends StatefulWidget {
+  final Note note;
+  final Color themeColor;
+
+  const NoteContainer({
+    super.key,
+    required this.note,
+    required this.themeColor,
+  });
+
+  @override
+  State<StatefulWidget> createState() => _NoteContainerState();
+}
+
+class _NoteContainerState extends State<NoteContainer> {
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(
+      context,
+    ).copyWith(colorScheme: ColorScheme.fromSeed(seedColor: widget.themeColor));
+    return Theme(
+      data: theme,
+      child: Card(
+        child: Row(children: [Text(widget.note.title), Text(widget.note.body)]),
+      ),
+    );
+  }
+}
