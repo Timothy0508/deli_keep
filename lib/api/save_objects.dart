6022d6b369c0bc2d8cb3a124d2ddc0110a6b7cc9
Timothy0508 <99249271+Timothy0508@users.noreply.github.com>
2025-04-18 15:12:29 +0800

    feat: add function to get all notes

--- lib/api/save_objects.dart
+++ lib/api/save_objects.dart
@@ -11,14 +11,24 @@ void saveNote(Note note) async {
   prefs.setStringList('noteIDList', noteIDList);
 }
 
-Future<String> getNote(String id) async {
+Future<Note> getNote(String id) async {
   var prefs = await SharedPreferences.getInstance();
   var result = prefs.getString(id) ?? 'Error fetching data'; //Safe check.
-  return result;
+  return Note.fromJson(result);
 }
 
 Future<List<String>> getNoteIDList() async {
   var prefs = await SharedPreferences.getInstance();
   var noteIDList = prefs.getStringList('noteIDList') ?? [];
   return noteIDList;
+}
+
+Future<List<Note>> getALlNotes() async {
+  var prefs = await SharedPreferences.getInstance();
+  List<String> noteIDList = await getNoteIDList();
+  List<Note> notes = List.generate(
+    noteIDList.length,
+    (i) => Note.fromJson(prefs.getString(noteIDList[i]) ?? ""),
+  );
+  return notes;
 }
